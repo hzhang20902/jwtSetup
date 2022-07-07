91b007fb90e3d2360661e0145b157fb184f665ec
hzhang20902 <85701939+hzhang20902@users.noreply.github.com>
2022-07-07 02:17:02 -0400

    created payload dir and Req and Resp classes

--- src/main/java/com/figgs/jwtexample/models/User.java
+++ src/main/java/com/figgs/jwtexample/models/User.java
@@ -50,6 +50,9 @@ public class User {
         this.id = id;
     }
 
+    public String getUsername(){ return username; }
+    public void setUsername(String username){ this.username = username; }
+
     public String getEmail() {
         return email;
     }
