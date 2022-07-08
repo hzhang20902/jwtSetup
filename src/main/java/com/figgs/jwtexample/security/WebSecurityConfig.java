1a8da03b218d1163bd11c2a83d6bf5d4bbc49de5
hzhang20902 <85701939+hzhang20902@users.noreply.github.com>
2022-07-08 01:49:51 -0400

    bean generates but now userdetailimpl is incorrect class or something

--- src/main/java/com/figgs/jwtexample/security/WebSecurityConfig.java
+++ src/main/java/com/figgs/jwtexample/security/WebSecurityConfig.java
@@ -1,18 +1,13 @@
 package com.figgs.jwtexample.security;
 
-import org.apache.tomcat.util.descriptor.web.ContextHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.config.ldap.EmbeddedLdapServerContextSourceFactoryBean;
-import org.springframework.security.config.ldap.LdapBindAuthenticationManagerFactory;
-import org.springframework.security.config.ldap.ContextSourceSettingPostProcessor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -71,7 +66,7 @@ public class WebSecurityConfig {
         return new AuthenticationManager() {
             @Override
             public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-                return null;
+                return authentication;
             }
         };
     }
